4594cd6692b1962fd991522f1b8c1854dd5040b9
mdjakirhossain-idb <jakir310773@gmail.com>
2024-08-13 18:56:23 +0600

    first commit

--- Laravel-9-CRUD/routes/web.php
+++ Laravel-9-CRUD/routes/web.php
@@ -1,21 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\almsController;
-use App\Http\Controllers\adminController;
 
+use App\Http\Controllers\ProductController;
 
-Route::get('/', [almsController::class, 'index'])->name('home');
-Route::get('/create', [almsController::class, 'create']);
-Route::post('/create', [almsController::class, 'store']);
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/login', [adminController::class, 'login'])->name('login');
-Route::post('/login', [adminController::class, 'logUser']);
-
-Route::get('/register', [adminController::class, 'register'])->name('register');
-Route::post('/register', [adminController::class, 'store']);
-
-Route::get('/show/{id}', [almsController::class, 'show']);
-
-Route::delete('/{id}', [almsController::class, 'destroy'])->name('destroy');
+Route::resource('products', ProductController::class);
